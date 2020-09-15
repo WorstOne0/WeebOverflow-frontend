c0461b20d1506840744a8fa44d43b39392d211a2
Worst One <luccagabriel12@hotmail.com>
2020-09-14 22:18:08 -0300

    Profile Page Iniciada

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,64 @@
 import React from "react";
 
-import { Container } from "./styles";
+import * as S from "./styles";
+
+import {
+  AiFillFacebook,
+  AiOutlineTwitter,
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillYoutube,
+  AiFillRedditCircle,
+} from "react-icons/ai";
+import { FaTwitch } from "react-icons/fa";
 
 import { NavBar } from "../../components";
 
 function Profile() {
+  const tempImg = require("../../assets/704387.png");
+  const UserImgTemp = require("../../assets/no_game_no_life-01-sora-older_brother-cloak-games-different.jpg");
+
   return (
     <NavBar>
-      <Container />
+      <S.Container>
+        <S.HeaderContainer>
+          <S.HeaderImg src={tempImg} />
+
+          <S.UserContainer>
+            <S.UserContent>
+              <S.UserImg url={UserImgTemp} />
+
+              <S.ContentTop>
+                <S.ContentTopLeft>
+                  <S.SocialMediaContainer>
+                    <AiFillFacebook className="Icon" className="Facebook" />
+                    <AiOutlineTwitter className="Icon" className="Twitter" />
+                    <AiFillGithub className="Icon" className="Github" />
+                    <AiFillInstagram className="Icon" className="Instagram" />
+                    <AiFillLinkedin className="Icon" className="Linkedin" />
+                    <AiFillYoutube className="Icon" className="Youtube" />
+                    <AiFillRedditCircle className="Icon" className="Reddit" />
+                    <FaTwitch className="Twitch" />
+                  </S.SocialMediaContainer>
+                </S.ContentTopLeft>
+                <S.ContentTopRight></S.ContentTopRight>
+              </S.ContentTop>
+
+              <S.UserTitleContainer>
+                <S.UserNick>Worst One</S.UserNick>
+                <S.UserName>@worstone</S.UserName>
+              </S.UserTitleContainer>
+            </S.UserContent>
+          </S.UserContainer>
+        </S.HeaderContainer>
+
+        <S.MainSection>
+          <S.Main></S.Main>
+
+          <S.SideBar></S.SideBar>
+        </S.MainSection>
+      </S.Container>
     </NavBar>
   );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,6 +1,242 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  height: calc(100vh - 10rem);
+export const Container = styled.section`
   width: 100%;
+  background: var(--color-background-dark);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const HeaderContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  position: relative;
+`;
+
+export const HeaderImg = styled.img`
+  height: 60%;
+  width: 100%;
+  background: purple;
+`;
+
+export const UserContainer = styled.div`
+  height: 50%;
+  width: calc(100% - 3rem);
+  border-radius: 0.8rem;
+  background: var(--color-dark);
+
+  position: absolute;
+  top: 73%;
+  left: 50%;
+
+  transform: translate(-50%, -50%);
+`;
+
+export const UserContent = styled.div`
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+
+  position: relative;
+`;
+
+export const UserImg = styled.div`
+  height: 18rem;
+  width: 18rem;
+  border-radius: 50%;
+
+  background-image: url(${(props) => props.url});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const ContentTop = styled.div`
+  height: 11rem;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ContentTopLeft = styled.div`
+  height: 100%;
+  width: calc(50% - 9rem);
+  padding: 0 2.5rem;
+
+  display: flex;
+  align-items: flex-start;
+`;
+
+export const SocialMediaContainer = styled.div`
+  padding: 2.5rem 0;
+
+  display: flex;
+  align-items: center;
+
+  color: var(--color-white);
+  font-size: 2rem;
+
+  .Facebook {
+    margin-right: 1rem;
+
+    cursor: pointer;
+
+    transition: color 0.3s;
+
+    :hover {
+      color: #4267b2;
+    }
+  }
+
+  .Twitter {
+    margin-right: 1rem;
+
+    cursor: pointer;
+
+    transition: color 0.3s;
+
+    :hover {
+      color: #1da1f2;
+    }
+  }
+
+  .Github {
+    margin-right: 1rem;
+
+    cursor: pointer;
+
+    transition: color 0.3s;
+
+    :hover {
+      color: #111;
+    }
+  }
+
+  .Instagram {
+    margin-right: 1rem;
+
+    cursor: pointer;
+
+    transition: color 0.3s;
+
+    :hover {
+      color: #e1306c;
+    }
+  }
+
+  .Linkedin {
+    margin-right: 1rem;
+
+    cursor: pointer;
+
+    transition: color 0.3s;
+
+    :hover {
+      color: #2867b2;
+    }
+  }
+
+  .Youtube {
+    margin-right: 1rem;
+
+    cursor: pointer;
+
+    transition: color 0.3s;
+
+    :hover {
+      color: #ff0000;
+    }
+  }
+
+  .Reddit {
+    margin-right: 1rem;
+
+    cursor: pointer;
+
+    transition: color 0.3s;
+
+    :hover {
+      color: #ff4500;
+    }
+  }
+
+  .Twitch {
+    margin-right: 1rem;
+
+    cursor: pointer;
+
+    transition: color 0.3s;
+
+    :hover {
+      color: #6441a5;
+    }
+  }
+`;
+
+export const ContentTopRight = styled.div`
+  height: 100%;
+  width: calc(50% - 9rem);
+  padding: 0 2.5rem;
+
+  display: flex;
+`;
+
+export const UserTitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  color: var(--color-white);
+`;
+
+export const UserNick = styled.p`
+  font-size: 3.2rem;
+`;
+
+export const UserName = styled.p`
+  font-size: 1.6rem;
+`;
+
+export const MainSection = styled.section`
+  width: 100%;
+  padding: 0 1.5rem;
+  background: var(--color-background-dark);
+
+  display: flex;
+`;
+
+export const Main = styled.main`
+  height: 100vh;
+  margin-right: 1.5rem;
+  background: var(--color-dark);
+
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const SideBar = styled.div`
+  height: 100vh;
+  background: var(--color-dark);
+
+  display: flex;
+  flex: 0 0 36rem;
 `;
