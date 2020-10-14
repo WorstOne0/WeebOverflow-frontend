1c506edd9430ec1efccae38359ac51068754dea1
Worst One <luccagabriel12@hotmail.com>
2020-10-14 08:07:31 -0300

    AWS Amplify .env

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-import { Container } from "./styles";
+import * as S from "./styles";
+import { BsArrowLeft } from "react-icons/bs";
+import { AiOutlineGoogle } from "react-icons/ai";
+import { FaFacebookF } from "react-icons/fa";
+
+import { InputText } from "../../components";
 
 const SignUp = () => {
-  return <Container />;
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [userName, setUsername] = useState("");
+
+  return (
+    <S.Container>
+      <S.Back onClick={() => history.goBack()}>
+        <BsArrowLeft />
+      </S.Back>
+
+      <S.Content>
+        <S.GoogleButton>
+          <AiOutlineGoogle className="Icon" /> Crie sua conta com o Google
+        </S.GoogleButton>
+        <S.FaceButton>
+          <FaFacebookF className="Icon" />
+          Crie sua conta com o Facebook
+        </S.FaceButton>
+
+        <S.Title>Crie sua conta</S.Title>
+
+        <S.InputWrapper>
+          <InputText
+            value={email}
+            setValue={(event) => {
+              event.preventDefault();
+
+              setEmail(event.target.value);
+            }}
+            name="E-mail"
+            borderColor={"var(--color-hover-darker)"}
+          />
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <InputText
+            value={password}
+            setValue={(event) => {
+              event.preventDefault();
+
+              setPassword(event.target.value);
+            }}
+            name="Password"
+            type="password"
+            borderColor={"var(--color-hover-darker)"}
+          />
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <InputText
+            value={confirmPassword}
+            setValue={(event) => {
+              event.preventDefault();
+
+              setConfirmPassword(event.target.value);
+            }}
+            name="Re-Password"
+            type="password"
+            borderColor={"var(--color-hover-darker)"}
+          />
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <InputText
+            value={userName}
+            setValue={(event) => {
+              event.preventDefault();
+
+              setUsername(event.target.value);
+            }}
+            name="Username"
+            borderColor={"var(--color-hover-darker)"}
+          />
+        </S.InputWrapper>
+        {/* Genero */}
+        {/* Aniversario */}
+
+        <S.Button>Criar conta</S.Button>
+      </S.Content>
+    </S.Container>
+  );
 };
 
 export default SignUp;
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100vh;
+  height: auto;
   width: 100%;
   background: var(--color-dark);
 
@@ -10,4 +10,100 @@ export const Container = styled.div`
   align-items: center;
 
   position: relative;
+`;
+
+export const Back = styled.div`
+  position: absolute;
+  top: 2rem;
+  left: 2rem;
+
+  font-size: 2.2rem;
+
+  cursor: pointer;
+`;
+
+export const Content = styled.div`
+  min-height: 100vh;
+  width: 40rem;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+
+  position: relative;
+`;
+
+export const Title = styled.h1`
+  margin: 4rem 0 3rem 0;
+
+  color: var(--color-white);
+  font-size: 2.8rem;
+`;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  margin-top: 3rem;
+`;
+
+export const Button = styled.button`
+  height: 5rem;
+  width: 100%;
+  margin-top: 4rem;
+  border: 1px solid var(--color-hover-darker);
+  border-radius: 0.8rem;
+  background: transparent;
+
+  transition: all 0.3s;
+  cursor: pointer;
+
+  :hover {
+    background: var(--color-primary);
+  }
+`;
+
+export const GoogleButton = styled.button`
+  height: 5rem;
+  width: 100%;
+  margin-top: 6rem;
+  border: 1px solid var(--color-white);
+  border-radius: 0.8rem;
+  background: transparent;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: all 0.3s;
+  cursor: pointer;
+
+  .Icon {
+    margin-right: 1rem;
+
+    font-size: 1.8rem;
+  }
+`;
+
+export const FaceButton = styled.button`
+  height: 5rem;
+  width: 100%;
+  margin-top: 2rem;
+  border: 1px solid #4267b2;
+  border-radius: 0.8rem;
+  background: transparent;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: all 0.3s;
+  cursor: pointer;
+
+  color: #4267b2;
+
+  .Icon {
+    margin-right: 1rem;
+
+    font-size: 1.8rem;
+  }
 `;
