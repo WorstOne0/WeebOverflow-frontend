f747c3722b5e47164334cd010dd8cfa80d058e01
Worst One <worstone90@gmail.com>
2022-07-13 17:27:34 -0300

    netfily

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -18,7 +18,7 @@ const Comments = () => {
           reaction: 10,
           user: { picture: UserImgTemp, username: "jikkai10" },
           date: "há 5 horas",
-          text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type",
+          text: "Lorem a Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type",
           comment: null,
         },
         {
